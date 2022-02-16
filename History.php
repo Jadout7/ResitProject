b8c0d25a013b0eb9f3cf7d94e9ba2d632cf635a5
Stef0002 <stefani.margaritova@student.nhlstenden.com>
2022-02-16 22:48:01 +0100

    Made History page a php file

--- History.php
+++ History.php
@@ -7,27 +7,9 @@
         <link rel="stylesheet" href="Main.css" type="text/css">
     </head>
     <body>
-        <header>
-            <div class="head">
-                <nav>
-                    <div class="head_left">
-                        <ul>
-                        <li><a href="#"><h3><b>NHL WEBSHOP</b></h3></a></li>
-                        </ul>
-                    </div>
-                    <div class="head_center">
-                        <ul>
-                        <li><textarea name="textarea" id="textarea" placeholder="Zoeken"></textarea></li>
-                        </ul>
-                    </div>
-                    <div class="head_left">
-                        <ul>
-                            <li><a href="#"><h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h3></a></li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        </header>
+        <?php
+        include 'header.php';
+        ?>
         <main>
             <div class="mainTitle">
                 <h1>Your order history</h1>
