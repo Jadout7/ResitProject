1515cb7b6d5557a342c2acae1bff416ae4c88100
Stef0002 <stefani.margaritova@student.nhlstenden.com>
2022-02-16 22:48:25 +0100

    Made the header an include

---
+++ header.php
@@ -0,0 +1,21 @@
+<header>
+    <div class="head">
+        <nav>
+            <div class="head_left">
+                <ul>
+                <li><a href="Main.php"><h3><b>NHL WEBSHOP</b></h3></a></li>
+                </ul>
+            </div>
+            <div class="head_center">
+                <ul>
+                <li><textarea name="textarea" id="textarea" placeholder="Zoeken"></textarea></li>
+                </ul>
+            </div>
+            <div class="head_left">
+                <ul>
+                    <li><a href="#"><h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h3></a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</header>
