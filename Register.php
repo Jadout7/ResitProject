6ad97821f42f8c314ff837a01659fd383fabb4ee
Stef0002 <stefani.margaritova@student.nhlstenden.com>
2022-02-17 17:17:13 +0100

    Added age check & some php checks

--- Register.php
+++ Register.php
@@ -9,6 +9,7 @@
     <body>
         <?php
         include 'header.php';
+        include 'Database.php';
         ?>
         <main>
             <div class="register">
@@ -36,6 +37,11 @@
                                 <label for='confirmPassword'>Confirm Password</label>
                                 <input type="password" name="confirmPassword">
                             </div>
+                            <div>
+                                <label for="age">Are you over 18 years old?</label><br>
+                                <input type="radio" name="age" value="ofAge">&nbsp;Yes, I'm over 18.<br/>
+                                <input type="radio" name="age" value="notOfAge">&nbsp;No, I'm under 18.<br/>
+                            </div>
                             <div class="log">
                                 <input type="submit" name="register" value="Create account" class="reg">
                                 <a href='Login.php'>Login</a>
@@ -45,9 +51,24 @@
                     <?php
                     $error = NULL;
                     if (isset($_POST['register'])) {
-                        if (!empty($_POST['firstName']) && !empty($_POST['lastName']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['confirmPassword'])) { //check if all fields have been filled
-                            if ($_POST['password'] == $_POST['confirmPassword']) {
+                        if (!empty($_POST['firstName']) && !empty($_POST['lastName']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['confirmPassword'])) { //check if all fields are filled
+                            if ($_POST['password'] == $_POST['confirmPassword']) { //check if the entered passwords are the same
+                                if (strlen(trim($_POST['password'])) > 6) { //check if the password is longer than 6 char.
+                                    $email = $_POST["email"];
+                                    if (filter_var($email, FILTER_VALIDATE_EMAIL)) { //validate the email format
+                                        $sql = "SELECT email FROM user WHERE email = ?"; //query command to search if email already exists
+                                        $result = $conn->query("SELECT email FROM user WHERE email = ?");
+                                        if($result->num_rows == 0) {
 
+                                        }else {
+                                            $error = "Email already exists.";
+                                        }
+                                    } else {
+                                        $error = "Invalid email.";
+                                    }
+                                }else {
+                                    $error = "Password must be longer than 6 characters!";
+                                }
                             }else{
                                 $error = "Passwords don't match!";
                             }
