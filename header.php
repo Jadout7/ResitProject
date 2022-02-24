254fc687cf8f9b0bf277dff2e71ad3188861df48
Jadooooon <jadout7@gmail.com>
2022-02-24 18:04:14 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,4 +1,18 @@
 <header>
+    <?php
+        session_start();
+        require_once './includes/RememberMe.php'
+        if(isset($_SESSION['type'])) {
+            if($_SESSION['type'] != 'customer') {
+                echo "<div class='pages'>";
+                echo "<a href='./Main.php'><h2>Home</h2></a>";
+                echo "</div>";
+                echo "<div class='pages'>";
+                echo "<a href='./Cart.php'><h2>Your Cart</h2></a>";
+                echo "</div>";
+            }
+        }
+    ?>
     <div class="head">
         <nav>
             <div class="head_left">
@@ -11,11 +25,20 @@
                 <li><textarea name="textarea" id="textarea" placeholder="Zoeken"></textarea></li>
                 </ul>
             </div>
-            <div class="head_left">
-                <ul>
-                    <li><a href="#"><h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h3></a></li>
-                </ul>
-            </div>
         </nav>
+    </div>
+    <div class="userInfo">
+        <?php
+            if($_SESSION['type'] == "admin"){
+                echo "<p><a href='./additem.php'>Maintenance</a></p>";
+            }
+            echo "<p><a href='./logout.php'>Logout</a></p>";
+            }else{
+            echo "<p><a href='./login.php'>Login</a></p>";
+            echo "<p><a href='./register.php'>Register</a></p>";
+            echo "<a href='./Cart.php'><h2>Your Cart</h2></a>";
+            echo "<a href='./History.php'><h2>Order History</h2></a>";
+            }
+        ?>
     </div>
 </header>
