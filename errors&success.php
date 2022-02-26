577c8a20c45452f03da0bf9becbca62ff3c4f4a8
Jadooooon <jadout7@gmail.com>
2022-02-26 12:15:52 +0100

    Errors and Success file

---
+++ errors&success.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Webshop</title>
+</head>
+<body>
+    <?php
+    include 'header.php';
+    ?>
+    <main>
+        <div class="bigBox">
+        <?php
+        if(isset($_GET['error'])) {
+            if($_GET['error'] == 'type') {
+            echo "<p class='warning'>You do not have access to that page.</p>";
+            }
+            elseif($_GET['error'] == 'login') {
+            echo "<p class='warning'>You are already logged in!</p>";
+            }
+            elseif($_GET['error'] == 'register') {
+            echo "<p class='warning'>Please log out before registering another account.</p>";
+            }
+            elseif($_GET['error'] == 'formdata') {
+            echo "<p class='warning'>Error occured, please try again!</p>";
+            }
+        }
+        if(isset($_GET['success'])) {
+            if($_GET['success'] == 'login') {
+            echo "<p class='success'>You are now logged into the service!</p>";
+            }
+            elseif($_GET['success'] == 'logout') {
+            echo "<p class='warning'>You are now logged out of the service.</p>";
+            }
+            elseif($_GET['success'] == 'ordered_item') {
+            echo "<p class='success'>Item(s) ordered successfully!</p>";
+            }
+        }
+        ?>
+    </main>
+</body>
+</html>
