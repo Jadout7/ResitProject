0a57c06cc04092fe3d9683a625230b11cea62700
Jadooooon <jadout7@gmail.com>
2022-02-26 12:56:10 +0100

    Update header.php

--- header.php
+++ header.php
@@ -23,6 +23,7 @@
                         echo "<li><a href="Cart.php"><h3>Your Cart</h3></a></li>";
                         echo "<li><a href="History.php"><h3>Order History</h3></a></li>";
                     }
+                    ?>
                 <li><a href="Login.php"><h3>Login</h3></a></li>
                 <li><a href="Register.php"><h3>Register</h3></a></li>
                 </ul>
