cb798a9a95e01b10087101d7309172c651d2762c
Jadooooon <jadout7@gmail.com>
2022-02-26 14:20:55 +0100

    Few Changes

--- errors&success.php
+++ errors&success.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
   <title>Webshop</title>
+  <link rel="stylesheet" href="Main.css" type="text/css">
 </head>
 <body>
     <?php
@@ -12,27 +13,34 @@
         <?php
         if(isset($_GET['error'])) {
             if($_GET['error'] == 'type') {
-            echo "<br><p class='warning'>You do not have access to that page.</p>";
+            echo "<br><p class='warning'>&nbsp;&nbsp;You do not have access to that page.</p>";
+            echo "<br><br><a href='./Main.php'><h3>&nbsp;&nbsp;Home</h3></a>";
             }
             elseif($_GET['error'] == 'login') {
-            echo "<br><p class='warning'>You are already logged in!</p>";
+            echo "<br><p class='warning'>&nbsp;&nbsp;You are already logged in!</p>";
+            echo "<br><br><a href='./Main.php'><h3>&nbsp;&nbsp;Home</h3></a>";
             }
             elseif($_GET['error'] == 'register') {
-            echo "<br><p class='warning'>Please log out before registering another account.</p>";
+            echo "<br><p class='warning'>&nbsp;&nbsp;Please log out before registering another account.</p>";
+            echo "<br><br><a href='./Main.php'><h3>&nbsp;&nbsp;Home</h3></a>";
             }
             elseif($_GET['error'] == 'formdata') {
-            echo "<br><p class='warning'>Error occured, please try again!</p>";
+            echo "<br><p class='warning'>&nbsp;&nbsp;Error occured, please try again!</p>";
+            echo "<br><br><a href='./Main.php'><h3>&nbsp;&nbsp;Home</h3></a>";
             }
         }
         if(isset($_GET['success'])) {
             if($_GET['success'] == 'login') {
-            echo "<br><p class='success'>You are now logged into the service!</p>";
+            echo "<br><p class='success'>&nbsp;&nbsp;You are now logged into the service!</p>";
+            echo "<br><br><a href='./Main.php'><h3>&nbsp;&nbsp;Home</h3></a>";
             }
             elseif($_GET['success'] == 'logout') {
-                echo "<br><p class='warning'>You are now logged out of the service.</p>";
+                echo "<br><p class='warning'>&nbsp;&nbsp;You are now logged out of the service.</p>";
+                echo "<br><br><a href='./Main.php'><h3>&nbsp;&nbsp;Home</h3></a>";
               }
             elseif($_GET['success'] == 'ordered_item') {
-            echo "<br><p class='success'>Item(s) ordered successfully!</p>";
+            echo "<br><p class='success'>&nbsp;&nbsp;Item(s) ordered successfully!</p>";
+            echo "<br><br><a href='./Main.php'><h3>&nbsp;&nbsp;Home</h3></a>";
             }
         }
         ?>
