46f9e2b35497e7162f5adce366cd3ae7264f3dce
Jadooooon <jadout7@gmail.com>
2022-02-26 12:28:21 +0100

    Update errors&success.php

--- errors&success.php
+++ errors&success.php
@@ -12,27 +12,24 @@
         <?php
         if(isset($_GET['error'])) {
             if($_GET['error'] == 'type') {
-            echo "<p class='warning'>You do not have access to that page.</p>";
+            echo "<br><p class='warning'>You do not have access to that page.</p>";
             }
             elseif($_GET['error'] == 'login') {
-            echo "<p class='warning'>You are already logged in!</p>";
+            echo "<br><p class='warning'>You are already logged in!</p>";
             }
             elseif($_GET['error'] == 'register') {
-            echo "<p class='warning'>Please log out before registering another account.</p>";
+            echo "<br><p class='warning'>Please log out before registering another account.</p>";
             }
             elseif($_GET['error'] == 'formdata') {
-            echo "<p class='warning'>Error occured, please try again!</p>";
+            echo "<br><p class='warning'>Error occured, please try again!</p>";
             }
         }
         if(isset($_GET['success'])) {
             if($_GET['success'] == 'login') {
-            echo "<p class='success'>You are now logged into the service!</p>";
-            }
-            elseif($_GET['success'] == 'logout') {
-            echo "<p class='warning'>You are now logged out of the service.</p>";
+            echo "<br><p class='success'>You are now logged into the service!</p>";
             }
             elseif($_GET['success'] == 'ordered_item') {
-            echo "<p class='success'>Item(s) ordered successfully!</p>";
+            echo "<br><p class='success'>Item(s) ordered successfully!</p>";
             }
         }
         ?>
