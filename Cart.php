1fe5ecbbb0ae92bf3c719be6eaf1999c73218147
Jadooooon <jadout7@gmail.com>
2022-03-01 14:09:54 +0100

    Update Cart.php

--- Cart.php
+++ Cart.php
@@ -7,8 +7,14 @@
         <link rel="stylesheet" href="Main.css" type="text/css">
     </head>
     <body>
-        <?php
+    <?php
             include 'header.php';
+        ?>
+        <main>
+            <div class="mainTitle">
+                <h1>Your cart</h1>
+            </div>
+        <?php
             include 'Database.php';
             $subtotal=0;
             $total=0;
@@ -25,30 +31,25 @@
                         mysqli_stmt_store_result($stmt);
                         while($attr=mysqli_stmt_fetch($stmt)) {
                         $subtotal=$quantity*$price;
+                        $total+=$subtotal;
         ?>
-        <main>
-            <div class="mainTitle">
-                <h1>Your cart</h1>
-            </div>
             <article>
                 <div class="productBox">
-                    <img src="<?php $attr['image'] ?>" alt="Product Image"/>
-                    <p><?php $attr['title'] ?></p>
-                    <p><?php $attr['quantity'] ?></p>
+                    <img src="<?php $attr['Pimage'] ?>" alt="Product Image"/>
+                    <p><?php $attr['Ptitle'] ?></p>
+                    <p><?php $attr['Pquantity'] ?></p>
                     <p><?php $subtotal ?></p>
+                    <p><?php echo "<h2>Subtotal:" .$subtotal. "</h2>";?>
                 </div>
             </article>
-            <div class="total">
-                <?php
-                $total+=$subtotal;
-                echo "<h2>Total:" .$total. "</h2>";
-                ?>
-            </div>
             <?php
                         }
                     }
                 }
 	        ?>
+            <div class="total">
+                <?php echo "<h2>Total:" .$total. "</h2>"; ?>
+            </div>
             <div class="UandC">
                 <input type="submit" name="update" value="Update">
                 <input type="submit" name="check" value="Checkout">
