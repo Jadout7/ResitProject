50513371df3aa6319fcc35fd1791561f0a707c8c
Jadooooon <jadout7@gmail.com>
2022-03-01 14:09:48 +0100

    Update History.php

--- History.php
+++ History.php
@@ -7,47 +7,44 @@
         <link rel="stylesheet" href="Main.css" type="text/css">
     </head>
     <body>
-        <?php
+    <?php
         include 'header.php';
+    ?>
+    <main>
+        <div class="mainTitle">
+            <h1>Your order history</h1>
+        </div>
+        <?php
+        $user_id=1;
+        $subtotal=0;
+        $total=0;
+            $sql = "select distinct o.order_id, i.price, oi.quantity
+                FROM item i
+                JOIN ordereditem oi ON oi.order_item_id = i.item_id
+                JOIN orders o ON oi.order_id = o.order_id
+                WHERE o.user_id=?;";
+                if($stmt = mysqli_prepare($conn, $sql)) {
+                    mysqli_stmt_bind_param($stmt, "i", $user_id);
+                    if(mysqli_stmt_execute($stmt)) {
+                        mysqli_stmt_bind_result($stmt, $id, $price, $quantity);
+                        mysqli_stmt_store_result($stmt);
+                        while($attr=mysqli_stmt_fetch($stmt)) {
+                            $subtotal=$quantity*$price;
+                            $total+=$subtotal;
+                            
         ?>
-        <main>
-            <div class="mainTitle">
-                <h1>Your order history</h1>
-            </div>
             <article>
                 <div class="orderBox">
-                    <h2>Order Number</h2>
-                    <h3>Total price: &euro;32,98</h3>
+                    <p>Order Number: <?php $attr['order_id']?></p>
+                    <h3>Total price: <?php echo"&euro;" .$total ?></h3>
                     <a href="#">Order details &gt;&gt;</a>
                 </div>
             </article>
-            <article>
-                <div class="orderBox">
-                    <h2>Order Number</h2>
-                    <h3>Total price: &euro;32,98</h3>
-                    <a href="#">Order details &gt;&gt;</a>
-                </div>
-            </article>
-            <article>
-                <div class="orderBox">
-                    <h2>Order Number</h2>
-                    <h3>Total price: &euro;32,98</h3>
-                    <a href="#">Order details &gt;&gt;</a>
-                </div>
-            </article>
-            <article>
-                <div class="orderBox">
-                    <h2>Order Number</h2>
-                    <h3>Total price: &euro;32,98</h3>
-                    <a href="#">Order details &gt;&gt;</a>
-                </div>
-            </article>
-            <div class="more">
-                <b><u><a href="#">More orders &gt;&gt;</a></u></b>
-            </div>
-            <?php
-            $sql="select oi.orderitemid, oi.orderid, oi.productdesc, oi.quantity from orderitems oi where oi.orderid = 1;";
-            ?>
         </main>    
+        <?php
+                    }
+                }
+            }
+	    ?>
     </body>
 </html>
