107791382240988f26c541bb7237d75f824ba8ea
Jadooooon <jadout7@gmail.com>
2022-03-02 10:51:45 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="Main.css" type="text/css">
 <header>
     <?php
-        require_once 'Remember.php'
+    require_once 'Remember.php'
     ?>
     <div class="head">
         <nav>
@@ -20,24 +20,26 @@
                     <?php 
                     if(isset($_SESSION['user_type'])) {
                         echo"<li><a><h6>Logged in as: <br>".$_SESSION['user_type']."</h6></a></li>";
-                        if($_SESSION['user_type'] = 'customer') {
-                            echo "<li><a href='./Cart.php'><h3>Your Cart</h3></a></li>";
-                            echo "<li><a href='History.php'><h3>Order History</h3></a></li>";
-                            echo "<li><a href='./Logout.php'><h3>Logout</h3></a></li>";
-                        }
-                        elseif($_SESSION['user_type'] = 'administrator') {
+                        if($_SESSION['user_type'] == 'administrator') {
                             echo "<li><a href='./Create.php'><h3>Add Product</h3></a></li>";
-                            echo "<li><a href='./Logout.php'><h3>Logout</h3></a></li>";
+                            echo "<li><a href='./Discount.php'><h3>Create Discount</h3></a></li>";
+                            echo "<li><a href='./Products.php'><h3>Products</h3></a></li>";
                         }
-                        elseif($_SESSION['user_type'] = 'orderpicker') {
+                        elseif($_SESSION['user_type'] == 'orderpicker') {
                             echo "<li><a href='./Orders.php'><h3>Orders</h3></a></li>";
-                            echo "<li><a href='./Logout.php'><h3>Logout</h3></a></li>"; 
                         }
+                        elseif($_SESSION['user_type'] == 'customer'){
+                            echo "<li><a href='./Products.php'><h3>Products</h3></a></li>";
+                            echo "<li><a href='./Cart.php'><h3>Cart</h3></a></li>";
+                            echo "<li><a href='./Discount.php'><h3>Order History</h3></a></li>";
+                        }
+                        echo "<li><a href='./Logout.php'><h3>Logout</h3></a></li>";
                     }
                     else{
                         echo "<li><a href='Login.php'><h3>Login</h3></a></li>";
                         echo "<li><a href='Register.php'><h3>Register</h3></a></li>";
                     }
+                    
                     ?>
                 </ul>
             </div>
