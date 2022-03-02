f5a4f6655783ef35d22ef22abf3801c99b69be02
Jadooooon <jadout7@gmail.com>
2022-03-02 10:51:41 +0100

    Update Login.php

--- Login.php
+++ Login.php
@@ -45,18 +45,18 @@
           $error = NULL;
           if(isset($_POST['login'])){
             if(!empty($_POST['email']) && !empty($_POST['password'])){
-              if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){ //validate email
+              if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){ //filters variable w email and validates it
                 $email = $_POST['email'];
-                $sql = "SELECT 'user_id', firstname, lastname, password, user_type FROM user WHERE email = ?"; //query to insert into database
-                if($stmt = mysqli_prepare($conn, $sql)){ //database parses, compiles, and performs query optimization and stores w/o executing
-                  mysqli_stmt_bind_param($stmt, "s", $email); //need to bind values to parameters
-                  if(mysqli_stmt_execute($stmt)){ //execute the statement
-                    mysqli_stmt_bind_result($stmt, $id, $firstname, $lastname, $password, $type); //bind results
+                $sql = "SELECT 'user_id', firstname, lastname, password, user_type FROM user WHERE email = ?"; 
+                if($stmt = mysqli_prepare($conn, $sql)){ //database compiles and performs query optimization and stores w/o executing
+                  mysqli_stmt_bind_param($stmt, "s", $email); //need to bind $email to parameters/? in the sql statement
+                  if(mysqli_stmt_execute($stmt)){ 
+                    mysqli_stmt_bind_result($stmt, $id, $firstname, $lastname, $password, $type); 
                     mysqli_stmt_store_result($stmt);
                     if(mysqli_stmt_num_rows($stmt) != 0){
                         while(mysqli_stmt_fetch($stmt)){
                           if(password_verify($_POST['password'], $password)) { //verify password
-                              $_SESSION['firstname'] = $firstname; //set session variables to use across pages
+                              $_SESSION['firstname'] = $firstname; //setting session vriables
                               $_SESSION['lastname'] = $lastname;
                               $_SESSION['sessionID'] = $id;
                               $_SESSION['email'] = $email;
