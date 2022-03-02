465500d97d0f9298ce2ad26695feb7791d51d9e6
Stef0002 <stefani.margaritova@student.nhlstenden.com>
2022-03-02 16:28:20 +0100

    Updated login
    The error messages now show in the login box, not under it

--- Login.php
+++ Login.php
@@ -35,57 +35,57 @@
                     <div class='log'>
                         <input type="submit" name="login" value="Login">
                         <a href='Register.php'>Register</a>
-                    </div>    
+                    </div>
                   </form>
+                    <?php
+                    $error = NULL;
+                    if(isset($_POST['login'])){
+                        if(!empty($_POST['email']) && !empty($_POST['password'])){
+                            if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){ //filters variable w email and validates it
+                                $email = $_POST['email'];
+                                $sql = "SELECT 'user_id', firstname, lastname, password, user_type FROM user WHERE email = ?";
+                                if($stmt = mysqli_prepare($conn, $sql)){ //database compiles and performs query optimization and stores w/o executing
+                                    mysqli_stmt_bind_param($stmt, "s", $email); //need to bind $email to parameters/? in the sql statement
+                                    if(mysqli_stmt_execute($stmt)){
+                                        mysqli_stmt_bind_result($stmt, $id, $firstname, $lastname, $password, $type);
+                                        mysqli_stmt_store_result($stmt);
+                                        if(mysqli_stmt_num_rows($stmt) != 0){
+                                            while(mysqli_stmt_fetch($stmt)){
+                                                if(password_verify($_POST['password'], $password)) { //verify password
+                                                    $_SESSION['firstname'] = $firstname; //setting session variables
+                                                    $_SESSION['lastname'] = $lastname;
+                                                    $_SESSION['sessionID'] = $id;
+                                                    $_SESSION['email'] = $email;
+                                                    $_SESSION['user_type'] = $type;
+                                                    header("location:./errors&success.php?success=login");
+                                                }else {
+                                                    $error = "<br>&nbsp;&nbsp;Incorrect password!";
+                                                }
+                                            }
+                                        }else {
+                                            $error = "<br>&nbsp;&nbsp;Unregistered email!";
+                                        }
+                                    }else {
+                                        echo "<br>&nbsp;&nbsp;Error executing query";
+                                        die(mysqli_error($conn));
+                                    }
+                                }else{
+                                    die(mysqli_error($conn));
+                                }
+                            }else {
+                                $error = "<br>&nbsp;&nbsp;Invalid email!";
+                            }
+                        }else {
+                            $error = "<br>&nbsp;&nbsp;Please fill in all the fields!";
+                        }
+                        if($error != NULL){ //echo error if the variable has been set
+                            echo "<div class='warning'>".$error."</div>";
+                        }
+                    }
+                    ?>
                 </div>
             </div>
           </div>
         </main>
-        <?php
-          $error = NULL;
-          if(isset($_POST['login'])){
-            if(!empty($_POST['email']) && !empty($_POST['password'])){
-              if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){ //filters variable w email and validates it
-                $email = $_POST['email'];
-                $sql = "SELECT 'user_id', firstname, lastname, password, user_type FROM user WHERE email = ?"; 
-                if($stmt = mysqli_prepare($conn, $sql)){ //database compiles and performs query optimization and stores w/o executing
-                  mysqli_stmt_bind_param($stmt, "s", $email); //need to bind $email to parameters/? in the sql statement
-                  if(mysqli_stmt_execute($stmt)){ 
-                    mysqli_stmt_bind_result($stmt, $id, $firstname, $lastname, $password, $type); 
-                    mysqli_stmt_store_result($stmt);
-                    if(mysqli_stmt_num_rows($stmt) != 0){
-                        while(mysqli_stmt_fetch($stmt)){
-                          if(password_verify($_POST['password'], $password)) { //verify password
-                              $_SESSION['firstname'] = $firstname; //setting session vriables
-                              $_SESSION['lastname'] = $lastname;
-                              $_SESSION['sessionID'] = $id;
-                              $_SESSION['email'] = $email;
-                              $_SESSION['user_type'] = $type;
-                              header("location:./errors&success.php?success=login");
-                          }else {
-                              $error = "<br>&nbsp;&nbsp;Incorrect password!";
-                          }
-                        }
-                    }else {
-                      $error = "<br>&nbsp;&nbsp;Unregistered email!";
-                    }
-                  }else {
-                      echo "<br>&nbsp;&nbsp;Error executing query";
-                      die(mysqli_error($conn));
-                  }
-                }else{
-                    die(mysqli_error($conn));
-                }
-              }else {
-                  $error = "<br>&nbsp;&nbsp;Invalid email!";
-              }
-            }else {
-                $error = "<br>&nbsp;&nbsp;Please fill in all the fields!";
-            }
-            if($error != NULL){ //echo error if the variable has been set
-                 echo "<div class='warning'>".$error."</div>";
-            }
-          }
-        ?>
     </body>
 </html>
