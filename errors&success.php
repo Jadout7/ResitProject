1ac14ef0bb4cdde0d4e4eb342e34e8332b04afac
Stef0002 <stefani.margaritova@student.nhlstenden.com>
2022-03-04 00:33:54 +0100

    Updated Create.php & errors&success.php
    Fixed file upload, added some new errors to erros&success.php and overall formatting

--- errors&success.php
+++ errors&success.php
@@ -24,8 +24,28 @@
                 echo "<br><p class='warning'>&nbsp;&nbsp;Error occured, please try again!</p>";
                 echo "<br><br><a href='./Main.php'><h3>&nbsp;&nbsp;Home</h3></a>";
             }
+            elseif($_GET['error'] == 'missingdata') {
+                echo "<br><p class='warning'>&nbsp;&nbsp;One or more inputs are missing!</p>";
+                echo "<br><br><a href='./Main.php'><h3>&nbsp;&nbsp;Home</h3></a>";
+            }
             elseif($_GET['error'] == 'saving'){
                 echo "<br><p class='warning'>&nbsp;&nbsp;You do not have access to that page.</p>";
+                echo "<br><br><a href='./Main.php'><h3>&nbsp;&nbsp;Home</h3></a>";
+            }
+            elseif($_GET['error'] == 'dimensions') {
+                echo "<p class='warning'>Image dimensions can't exceed 500x500!</p>";
+                echo "<br><br><a href='./Main.php'><h3>&nbsp;&nbsp;Home</h3></a>";
+            }
+            elseif($_GET['error'] == 'imagetype') {
+                echo "<p class='warning'>Invalid image type! Only png, jpeg and jpg are permitted.</p>";
+                echo "<br><br><a href='./Main.php'><h3>&nbsp;&nbsp;Home</h3></a>";
+            }
+            elseif($_GET['error'] == 'size') {
+                echo "<p class='warning'>Image should not exceed 3MB.</p>";
+                echo "<br><br><a href='./Main.php'><h3>&nbsp;&nbsp;Home</h3></a>";
+            }
+            elseif($_GET['error'] == 'filename') {
+                echo "<p class='warning'>File name must not exceed 35 Characters.</p>";
                 echo "<br><br><a href='./Main.php'><h3>&nbsp;&nbsp;Home</h3></a>";
             }
         }
