93c1ef585fadfe153bc852ba4c72be20110e71a5
Jadooooon <jadout7@gmail.com>
2022-03-03 17:37:48 +0100

    Update Products.php

--- Products.php
+++ Products.php
@@ -16,11 +16,11 @@
     ?>
     <article>
         <div class = 'article_product'>
-            <img src = "./upload/<?php $attr['image']?>" alt = "Product Image">
-            <h4><?php echo $attr['title'] ?></h4>
-            <p><?php echo $attr['description'] ?></p>
-            <p><b><i><?php echo $attr['category'] ?></i></b></p>
-            <h4><?php echo "Price: &euro;" .$attr['price'] ?></h4>
+            <img src = "./upload/<?php echo $attr['image']?>" alt = "Product Image"><br><br>
+            <h4><?php echo $attr['title']?></h4><br>
+            <p><?php echo $attr['description'] ?></p><br>
+            <p><b><i><?php echo $attr['category'] ?></i></b></p><br>
+            <h4><?php echo "Price: &euro;" .$attr['price'] ?></h4><br>
             <div class = 'log'>
                 <input type="submit" value="Add to Cart">
             </div>
