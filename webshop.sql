7425efc65d8515759579e390c671348b9f6a7805
Jadooooon <jadout7@gmail.com>
2022-03-03 17:36:33 +0100

    Update webshop.sql

--- webshop.sql
+++ webshop.sql
@@ -89,7 +89,7 @@ CREATE TABLE `item` (
   `description` varchar(300) NOT NULL,
   `category` varchar(20) NOT NULL,
   `price` float(11) NOT NULL,
-  `image` VARBINARY(8000) DEFAULT NULL
+  `image` varchar(60) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
