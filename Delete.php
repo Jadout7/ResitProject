f4c7ebe66dd1cc0ba6bab4f81a611072af81670e
Jadooooon <jadout7@gmail.com>
2022-03-04 16:39:01 +0100

    Update Delete.php

--- Delete.php
+++ Delete.php
@@ -7,12 +7,16 @@ include 'header.php';
     include 'Database.php';
     if (isset($_GET['order_id'])) {
         $id = $_GET['order_id'];
-        $query = "DELETE FROM `ordereditem` WHERE order_Id = '$id'";
-        $run = mysqli_query($conn, $query);
-        if ($run) {
-            header("Location: ./errors&success.php?success=deleted");
-        } else {
-            echo "Error: " . mysqli_error($conn);
+        $sql = "DELETE FROM `ordereditem` WHERE order_id = $id";
+        if ($stmt = mysqli_prepare($conn, $sql)) {
+            if (mysqli_stmt_execute($stmt)) {
+                $result = mysqli_stmt_get_result($stmt);
+                while ($result==true){
+                    header("Location: ./errors&success.php?success=deleted");
+                }             
+            } else {
+                echo "Error: " . mysqli_error($conn);
+            }
         }
     }
     ?>
