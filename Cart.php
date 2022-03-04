a57d36828033da03efe3215b13850a7fb8bc5aa7
Jadooooon <jadout7@gmail.com>
2022-03-04 09:11:35 +0100

    Update Cart.php

--- Cart.php
+++ Cart.php
@@ -14,15 +14,6 @@
     <main>
         <div class="mainTitle">
             <h1>Your cart</h1>
-            <form method="post" action=<?php echo $_SERVER['PHP_SELF'];?> enctype="multipart/form-data">
-            <div class="orderBy">
-                <select name="orderBy">
-                    <option value="PriceAscending">Price Ascending</option>
-                    <option value="PriceDescending">Price Descending</option>
-                    <option value="NameAscending">Name Ascending</option>
-                    <option value="NameDescending">Name Descending</option>
-                </select>
-            </div>
         </div>
         <?php
             $user_id=2;
@@ -45,8 +36,16 @@
             <div class="productBox">
                 <img src = "./upload/<?php echo $attr['image']?>" alt = "Product Image">
                 <h2><?php echo"Product : " .$attr['title']; ?></h2>
-                <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>" enctype="multipart/form-data">
-                <h3>Quantity: <input type="number" name="quantity" min="1" max="100"></h3>
+
+                <form method="post" action="Delete.php">
+                <input type="hidden" name="item_id" value="<?php echo $c['item_id']; ?>">
+                <input type="submit" value="Delete">
+                </form>
+
+                <form method="post" action="Update.php">
+                    <input type="number" name="quantity" min="1" value="<?php echo $c['quantity']; ?>">
+                    <input type="hidden" name="item_id" value="<?php echo $c['item_id']; ?>">
+                    <input type="submit" value="Update">
                 </form>
                 <h3><?php echo "Subtotal : &euro; " .$subtotal. ".00";?></h3>
             </div>
@@ -60,57 +59,10 @@
             <?php echo "<h2>Total: &euro; " .$total. ".00</h2>"; ?>
          </div>
         <div class="UandC">
-            <input type="submit" name="update" value="Update">
-            <input type="submit" name="check" value="Checkout">
+            <form method="post" action="Checkout.php">
+            <input type="submit" name="Checkout" value="Checkout">
+            </form>
         </div>
-        <?php
-            if(isset ($_POST['Update'])){
-                if($_POST['orderBy'] == "PriceAscending"){
-                    $sql = "select i.image, i.title, oi.quantity, i.price FROM item i JOIN ordereditem oi ON oi.order_item_id = i.item_id JOIN orders o ON oi.order_id = o.order_id WHERE o.user_id=? order by price asc;";
-                    if ($stmt = mysqli_prepare($conn, $sql)) {
-                        if (mysqli_stmt_execute($stmt)) {
-                            }else{
-                                echo mysqli_error($conn);
-                            }
-                    }else{
-                        echo mysqli_error($conn);
-                    }
-                }
-                elseif($_POST['orderBy'] == "PriceDescending"){
-                    $sql = "select i.image, i.title, oi.quantity, i.price FROM item i JOIN ordereditem oi ON oi.order_item_id = i.item_id JOIN orders o ON oi.order_id = o.order_id WHERE o.user_id=? order by price desc;";
-                    if ($stmt = mysqli_prepare($conn, $sql)) {
-                        if (mysqli_stmt_execute($stmt)) {
-                            }else{
-                                echo mysqli_error($conn);
-                            }
-                    }else{
-                        echo mysqli_error($conn);
-                    }
-                }
-                elseif($_POST['orderBy'] == "NameAscending"){
-                    $sql = "select i.image, i.title, oi.quantity, i.price FROM item i JOIN ordereditem oi ON oi.order_item_id = i.item_id JOIN orders o ON oi.order_id = o.order_id WHERE o.user_id=? order by title asc;";
-                    if ($stmt = mysqli_prepare($conn, $sql)) {
-                        if (mysqli_stmt_execute($stmt)) {
-                            }else{
-                                echo mysqli_error($conn);
-                            }
-                    }else{
-                        echo mysqli_error($conn);
-                    }
-                }
-                elseif($_POST['orderBy'] == "NameDescending"){
-                    $sql = "select i.image, i.title, oi.quantity, i.price FROM item i JOIN ordereditem oi ON oi.order_item_id = i.item_id JOIN orders o ON oi.order_id = o.order_id WHERE o.user_id=? order by title desc;";
-                    if ($stmt = mysqli_prepare($conn, $sql)) {
-                        if (mysqli_stmt_execute($stmt)) {
-                            }else{
-                                echo mysqli_error($conn);
-                            }
-                    }else{
-                        echo mysqli_error($conn);
-                    }
-                }
-            }
-        ?>
         </main>
     </body>
 </html>
