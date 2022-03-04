678b3cd693683dd0241e421fa5b8f63de3398ce7
liafernando <91317153+liafernando@users.noreply.github.com>
2022-03-04 12:17:43 +0100

    Add files via upload

---
+++ Homepage/Homepage.php
@@ -0,0 +1,98 @@
+<DOCTYPE html>
+<html lanh=en>
+<head>
+    <meta charset="UTF-8">
+	<Title>Homepage</Title>
+	<meta name="viewpoint" content="width=device-width,initial-scale=1.0">
+	<link rel="stylesheet" href="style.css" type="text/css">
+</head>
+   <body>
+   	<?php 
+        include 'header.php';
+    ?>
+   	<div class="center">
+   			<p>select one of the following categories<p>
+   			<ul>
+   				<div><a href="#">phones</a> -computers - components - Smart watches - Smart home</div>
+   			</ul>
+   	</div>
+    
+ <div class="center2">
+   	<h2>Found Products</h2>
+   </div>
+   	<div class="box1">
+   		<div class='pic'>
+   	    <img src="iphone13.png" alt="iphone">
+   	    <div class="desc">€25,99</div>
+   		</div>
+   		<div>
+   		</div>
+   	</div>
+   	<div class="box2">
+   		<div class='pic'>
+   	    <img src="iphone13.png" alt="iphone">
+   	    <div class="desc">€25,99</div>
+   		</div>
+   		<div>
+   			
+   		</div>
+   	</div>
+   	<div class="box3">
+   		<div class='pic'>
+   	    <img src="iphone13.png" alt="iphone">
+   	    <div class="desc">€25,99</div>
+   		</div>
+   		<div>
+   			
+   		</div>
+   	</div>
+   	<div class="box4">
+   		<div class='pic'>
+   	    <img src="iphone13.png" alt="iphone">
+   	    <div class="desc">€25,99</div>
+   		</div>
+   		<div>
+   			
+   		</div>
+   	</div>
+
+   	<br>
+      <br>
+
+   	<div class="box5">
+   		<div class='pic'>
+   	    <img src="iphone13.png" alt="iphone">
+   	    <div class="desc">€25,99</div>
+   		</div>
+   		<div>
+   			
+   		</div>
+   	</div>
+   	<div class="box6">
+   		<div class='pic'>
+   	    <img src="iphone13.png" alt="iphone">
+   	    <div class="desc">€25,99</div>
+   		</div>
+   		<div>
+   			
+   		</div>
+   	</div>
+   	<div class="box7">
+   		<div class='pic'>
+   	    <img src="iphone13.png" alt="iphone">
+   	    <div class="desc">€25,99</div>
+   		</div>
+   		<div>
+   			
+   		</div>
+   	</div>
+   	<div class="box8">
+   		<div class='pic'>
+   	    <img src="iphone13.png" alt="iphone">
+   	    <div class="desc">€25,99</div>
+   		</div>
+   		<div>
+   			
+   		</div>
+   	</div>
+</div>
---
+++ Homepage/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Order History</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style1.css" type="text/css">
+    </head>
+    <body>
+ <header>
+            <div class="head">
+                <nav>
+                    <div class="head_left">
+                        <ul>
+                        <li><a href="#"><h3><b>NHL WEBSHOP</b></h3></a></li>
+                        </ul>
+                    </div>
+                    <div class="head_center">
+                        <ul>
+                        <li><textarea name="textarea" id="textarea" placeholder="Zoeken"></textarea></li>
+                        </ul>
+                    </div>
+                    <div class="head_left">
+                        <ul>
+                            <div id="links">
+                            <li><a href="Login.html"><h3><b>Login</b></h3></a></li>
+                            </div>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    </body>
+</html>
