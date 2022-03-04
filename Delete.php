afd8176242a4e58880c8cf4e08fb7219c68ab460
Jadooooon <jadout7@gmail.com>
2022-03-04 16:31:07 +0100

    Update Delete.php

--- Delete.php
+++ Delete.php
@@ -1,32 +1,19 @@
-<body>
-    <?php
-    include 'header.php';
+</body>
+<?php
+include 'header.php';
+?>
+<main>
+    <?php   //selects Id from userId and deletes it
     include 'Database.php';
-    ?>
-    <main>
-        <?php   //selects Id from userId and deletes it
-    include 'conn.php';
-    if (isset($_GET['item_id'])) {
-        $id = $_GET['item_id'];
-        $query = "DELETE FROM `ordereditem` WHERE item_Id = '$id'";
+    if (isset($_GET['order_id'])) {
+        $id = $_GET['order_id'];
+        $query = "DELETE FROM `ordereditem` WHERE order_Id = '$id'";
         $run = mysqli_query($conn, $query);
         if ($run) {
-            header('location:./Cart.php');
+            header("Location: ./errors&success.php?success=deleted");
         } else {
             echo "Error: " . mysqli_error($conn);
         }
     }
     ?>
-        <?php
-        /*
-        if (isset($POST['Delete'])) {
-            $sql = "DELETE FROM `orders` WHERE `order_id` = ? && `status` == 'Is Received';";
-            mysqli_stmt_bind_param($stmt, "i", $orderid);
-            if (mysqli_stmt_execute($stmt)) {
-                header("Location: ./History.php");
-            } else {
-                echo "execute statement failed";
-            }
-        }
-        */
-        ?>
+    </body>
