bb6da9789268789e76637b010faa164c2d838a60
Jadooooon <jadout7@gmail.com>
2022-03-04 16:01:21 +0100

    Update webshop.sql

--- webshop.sql
+++ webshop.sql
@@ -20,6 +20,10 @@ SET time_zone = "+00:00";
 --
 -- Database: `webshop`
 --
+CREATE DATABASE IF NOT EXISTS `webshop` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `webshop`;
+--
+--
 
 -- --------------------------------------------------------
 
